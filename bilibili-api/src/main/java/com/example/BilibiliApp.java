8571f2d0ec7c1d6de59a602310ea4e895f135d9b
Shuming Zhang <chriszhangshu@gmail.com>
2025-08-10 21:19:31 -0400

    Add project source

---
+++ bilibili-api/src/main/java/com/example/BilibiliApp.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BilibiliApp {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(BilibiliApp.class, args);
+    }
+}
---
+++ bilibili-service/src/main/java/com/example/bilibili/service/DemoService.java
@@ -0,0 +1,17 @@
+package com.example.bilibili.service;
+import com.example.bilibili.dao.DemoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class DemoService {
+    @Autowired
+    // help to add dependency relations
+    private DemoDao demoDao;
+
+    public Map<String, Object> query(Long id){
+        return demoDao.query(id);
+    }
+}
