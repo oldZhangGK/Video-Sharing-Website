edf1ef21d62863da28e2b1a25aba541ce1418002
Shuming Zhang <chriszhangshu@gmail.com>
2025-08-12 18:27:33 -0400

    Added Authentification Following ability to users which will add liked video to its favorite group.

--- bilibili-service/src/main/java/com/example/bilibili/service/UserService.java
+++ bilibili-service/src/main/java/com/example/bilibili/service/UserService.java
@@ -137,4 +137,8 @@ public class UserService {
         userInfo.setUpdateTime(new Date());
         userDao.updateUserInfos(userInfo);
     }
+
+    public User getUserById(Long followingId) {
+        return userDao.getUserById(followingId);
+    }
 }
